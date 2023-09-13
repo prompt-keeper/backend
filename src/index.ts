f93ea9453892002cee0d080997a1bb57fb6c6c48
Nguyen Tran <merlinvn@gmail.com>
2023-09-12 23:53:14 -0400

    add swagger, prisma

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+app.use(swagger());
+
+app.get("/", () => "Hello Elysia").listen(3000);
+
+app.post("/sign-in", ({ body }) => body, {
+  body: t.Object(
+    {
+      username: t.String(),
+      password: t.String(),
+    },
+    {
+      description: "Expected an username and password",
+    },
+  ),
+  detail: {
+    summary: "Sign in the user",
+    tags: ["authentication"],
+  },
+});
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
